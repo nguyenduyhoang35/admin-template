f3e91febc1b6fb0aa93ed8be3aac49c6801a6005
Hoang Nguyen 7 <hoang.nguyen7@sotatek.com>
2023-05-25 19:12:32 +0700

    demo currency-fomatter

--- src/CurrentChildren.js
+++ src/CurrentChildren.js
@@ -1,15 +1,18 @@
 import { Grid } from "@react-md/utils";
 import StandardTable from "./components/StandardTable";
+import { CurrencyFormat } from "currency-fomatter"
+import { Input } from "antd";
 
 export function CurrentChildren({ route }) {
 	const data = {
 		list: [{}, {}, {}],
-		pagination: {},
+		pagination: null,
 	};
-  
+
 	return (
 		<Grid columns={1}>
 			<StandardTable data={data} />
+			<CurrencyFormat customInput={Input} fixedDecimalScale={true} decimalScale={3}/>
 		</Grid>
 	);
 }
