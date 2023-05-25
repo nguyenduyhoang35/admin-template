006224bf0efbc6cb31bfbfff69d36052336399ad
Hoang Nguyen 7 <hoang.nguyen7@sotatek.com>
2023-05-25 15:20:16 +0700

    table

--- src/CurrentChildren.js
+++ src/CurrentChildren.js
@@ -1,11 +1,15 @@
 import { Grid } from "@react-md/utils";
+import StandardTable from "./components/StandardTable";
 
-export function CurrentChildren({
-  route,
-}) {
-  return (
-    <Grid columns={1}>
-      {route}
-    </Grid>
-  );
+export function CurrentChildren({ route }) {
+	const data = {
+		list: [{}, {}, {}],
+		pagination: {},
+	};
+  
+	return (
+		<Grid columns={1}>
+			<StandardTable data={data} />
+		</Grid>
+	);
 }
---
+++ src/components/StandardTable/index.js
@@ -0,0 +1,127 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { PureComponent, Fragment } from 'react';
+import { Table, Alert } from 'antd';
+import styles from './style.scss';
+
+function initTotalList(columns) {
+  const totalList = [];
+  (columns|| []).forEach(column => {
+    if (column.needTotal) {
+      totalList.push({ ...column, total: 0 });
+    }
+  });
+  return totalList;
+}
+
+class StandardTable extends PureComponent {
+  constructor(props) {
+    super(props);
+    const { columns } = props;
+    const needTotalList = initTotalList(columns);
+
+    this.state = {
+      selectedRowKeys: [],
+      needTotalList,
+    };
+  }
+
+  static getDerivedStateFromProps(nextProps) {
+    // clean state
+    if ((nextProps.selectedRows|| []).length === 0) {
+      const needTotalList = initTotalList(nextProps.columns);
+      return {
+        selectedRowKeys: [],
+        needTotalList,
+      };
+    }
+    return null;
+  }
+
+  handleRowSelectChange = (selectedRowKeys, selectedRows) => {
+    let { needTotalList } = this.state;
+    needTotalList = needTotalList.map(item => ({
+      ...item,
+      total: selectedRows.reduce((sum, val) => sum + parseFloat(val[item.dataIndex], 10), 0),
+    }));
+
+    const { onSelectRow } = this.props;
+    if (onSelectRow) {
+      onSelectRow(selectedRows);
+    }
+
+    this.setState({ selectedRowKeys, needTotalList });
+  };
+
+  handleTableChange = (pagination, filters, sorter) => {
+    const { onChange } = this.props;
+    if (onChange) {
+      onChange(pagination, filters, sorter);
+    }
+  };
+
+  cleanSelectedKeys = () => {
+    this.handleRowSelectChange([], []);
+  };
+
+  render() {
+    const { selectedRowKeys, needTotalList } = this.state;
+    const { data = {}, rowKey, noSelected, ...rest } = this.props;
+    const { list = [], pagination } = data;
+
+    const paginationProps = {
+      showSizeChanger: true,
+      showQuickJumper: true,
+      ...pagination,
+    };
+
+    const rowSelection = {
+      selectedRowKeys,
+      onChange: this.handleRowSelectChange,
+      getCheckboxProps: record => ({
+        disabled: record.disabled,
+      }),
+    };
+    const props = !noSelected && rowSelection;
+
+    return (
+      <div className={styles.standardTable}>
+        <div className={styles.tableAlert}>
+          {selectedRowKeys.length > 0 && (
+            <Alert
+              message={
+                <Fragment>
+                  Đã chọn <a style={{ fontWeight: 600 }}>{selectedRowKeys.length}</a>{' '}
+                  dòng&nbsp;&nbsp;
+                  {needTotalList.map(item => (
+                    <span style={{ marginLeft: 8 }} key={item.dataIndex}>
+                      {item.title}
+                      Tổng&nbsp;
+                      <span style={{ fontWeight: 600 }}>
+                        {item.render ? item.render(item.total) : item.total}
+                      </span>
+                    </span>
+                  ))}
+                  <a onClick={this.cleanSelectedKeys} style={{ marginLeft: 24 }}>
+                    Bỏ chọn hết
+                  </a>
+                </Fragment>
+              }
+              type="info"
+              showIcon
+            />
+          )}
+        </div>
+        <Table
+          rowKey={rowKey || '_id'}
+          {...props}
+          dataSource={list}
+          pagination={paginationProps}
+          onChange={this.handleTableChange}
+          {...rest}
+        />
+      </div>
+    );
+  }
+}
+
+export default StandardTable;
